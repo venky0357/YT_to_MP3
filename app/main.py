3f4cfdd46c2a1f81daec39a43f026e11fd94b328
venkatesh <pulukurivenkatesh02@gmail.com>
2026-03-03 15:00:42 +0530

    new change Signature issue

--- app/main.py
+++ app/main.py
@@ -1,3 +1,23 @@
+# from fastapi import FastAPI, Query
+# from fastapi.responses import JSONResponse
+# import subprocess
+
+# app = FastAPI()
+
+# @app.get("/get-url")
+# def get_audio_url(q: str = Query(..., description="Search query")):
+#     try:
+#         command = ["yt-dlp","--cookies", "cookies.txt", "-f", "bestaudio", "--get-url", f"ytsearch:{q}"]
+#         result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+#         if result.returncode != 0:
+#             return JSONResponse(status_code=500, content={"error": result.stderr.strip()})
+
+#         url = result.stdout.strip().split("\n")[0]
+#         return {"url": url}
+
+#     except Exception as e:
+#         return JSONResponse(status_code=500, content={"error": str(e)})
 from fastapi import FastAPI, Query
 from fastapi.responses import JSONResponse
 import subprocess
@@ -7,13 +27,30 @@ app = FastAPI()
 @app.get("/get-url")
 def get_audio_url(q: str = Query(..., description="Search query")):
     try:
-        command = ["yt-dlp","--cookies", "cookies.txt", "-f", "bestaudio", "--get-url", f"ytsearch:{q}"]
-        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        command = [
+            "yt-dlp",
+            "-f", "bestaudio/best",
+            "--no-playlist",
+            "--extractor-args", "youtube:player_client=android",
+            "--get-url",
+            f"ytsearch1:{q}"
+        ]
+
+        result = subprocess.run(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
 
         if result.returncode != 0:
-            return JSONResponse(status_code=500, content={"error": result.stderr.strip()})
+            return JSONResponse(
+                status_code=500,
+                content={"error": result.stderr.strip()}
+            )
 
         url = result.stdout.strip().split("\n")[0]
+
         return {"url": url}
 
     except Exception as e:
