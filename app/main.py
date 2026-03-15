e34a8e8627357fac6f7a673a4a4dce59ae6c7e00
Venkatesh <107678214+venky0357@users.noreply.github.com>
2026-03-15 16:05:49 +0530

    Refactor get_audio_url to use cookies from browser

--- app/main.py
+++ app/main.py
@@ -5,9 +5,16 @@ import subprocess
 app = FastAPI()
 
 @app.get("/get-url")
-def get_audio_url(q: str = Query(..., description="Search query")):
+def get_audio_url(q: str = Query(...)):
     try:
-        command = ["yt-dlp","--cookies", "cookies.txt", "-f", "bestaudio", "--get-url", f"ytsearch:{q}"]
+        command = [
+            "yt-dlp",
+            "--cookies-from-browser", "chrome",
+            "-f", "bestaudio",
+            "--get-url",
+            f"ytsearch1:{q}"
+        ]
+
         result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
         if result.returncode != 0:
@@ -18,44 +25,3 @@ def get_audio_url(q: str = Query(..., description="Search query")):
 
     except Exception as e:
         return JSONResponse(status_code=500, content={"error": str(e)})
-
-# ---------------------------
-# from fastapi import FastAPI, Query
-# from fastapi.responses import JSONResponse
-# import subprocess
-
-# app = FastAPI()
-
-# @app.get("/get-url")
-# def get_audio_url(q: str = Query(..., description="Search query")):
-#     try:
-#         command = [
-#             "yt-dlp",
-#             "--cookies", "cookies.txt",
-#             "--extractor-args", "youtube:player_client=web",
-#             "--js-runtime", "node",
-#             "-f", "bestaudio/best",
-#             "--no-playlist",
-#             "--get-url",
-#             f"ytsearch1:{q}"
-#         ]
-
-#         result = subprocess.run(
-#             command,
-#             stdout=subprocess.PIPE,
-#             stderr=subprocess.PIPE,
-#             text=True
-#         )
-
-#         if result.returncode != 0:
-#             return JSONResponse(
-#                 status_code=500,
-#                 content={"error": result.stderr.strip()}
-#             )
-
-#         url = result.stdout.strip().split("\n")[0]
-
-#         return {"url": url}
-
-#     except Exception as e:
-#         return JSONResponse(status_code=500, content={"error": str(e)})
